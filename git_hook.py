77c25e089c86a9a49c436e9a19b95ea1866be249
exa160 <sfy111@hotmail.com>
2022-04-02 15:02:48 +0800

    update port set

--- git_hook.py
+++ git_hook.py
@@ -1,5 +1,5 @@
 import web
-import os
+import os, sys
 from git import Repo
 import json
 
@@ -8,15 +8,15 @@ app = web.application(urls, globals())
 class update:
     def POST(self):
         i = json.loads(web.data())
-        if i.token == 'mxu3nv74oiy7vi34n89xnf982bv824tdf34':
-            dirfile = ''
+        if i['token'] == 'mxu3nv74oiy7vi34n89xnf982bv824tdf34':
+            dirfile = os.path.abspath('')
             repo = Repo(dirfile)
             g = repo.git
             i = 0
             while i < 5:
                 i += 1
                 try:
-                    g.push()
+                    g.pull()
                 except Exception as e:
                     print(e)
                 else:
@@ -24,4 +24,6 @@ class update:
                     break
 
 if __name__ == '__main__':
+    sys.argv.append('10001')
     app.run()
+
